4b8388afb015afd96fa32e0a0e457ac082af3c12
Dmitry Zhuk <dmitryzhuk@gmail.com>
2014-05-28 12:50:42 +0300

    Updated version of Idea

--- test/main/scala/org/abovobo/search/IntersectionTest.scala
+++ test/main/scala/org/abovobo/search/IntersectionTest.scala
@@ -10,11 +10,77 @@
 
 package org.abovobo.search
 
-import scala.util.Random
-import scala.collection.mutable
-
 /** Tests propagation parameters calculator (alpha,tau) */
 object IntersectionTest extends App {
+
+
+  /*
+  // fixed size of D1 subset
+  val D2 = 50
+  // number of peaks on D2 distribution
+  val points = 10
+  // width of the curve
+  val width = D2 / points
+  // probability value interpreted as the end of the `bell`
+  val threshold = 10E-3
+
+  for (i <- 0 until points) {
+    val peak = width * (points - i) - width / 2
+    val right = width * (points - i)
+    val left = width * (points - i - 1)
+
+    def shape(data: IndexedSeq[Double]): Tuple3[Long, Long, Long] = {
+      var peak0 = -1
+      var peakval = 0.0
+      var left0 = -1
+      var leftval = 0.0
+      var right0 = -1
+      var rightval = 0.0
+      for (k <- 0 to D2) {
+        if (data(k) > peakval) {
+          peakval = data(k)
+          peak0 = k
+          if (data(k) < threshold) {
+            leftval = data(k)
+            left0 = k
+          }
+        } else {
+          if (data(k) > threshold) {
+            rightval = data(k)
+            right0 = k
+          }
+        }
+      }
+      (peak0, left0, right0)
+    }
+
+
+    var matched = false
+    var N = D2 * 2
+    var D1 = (D2 * 1.5).toLong
+
+    while (!matched) {
+
+      val p0 = p(N, D1, D2)
+      val shape0 = shape(p0)
+
+      println(shape0 + ";" + D1 + ";" + N)
+      if (shape0._1 > peak) {
+        N = N + Math.max(1, (N * 0.01).toInt)
+      } else if (shape0._1 < peak) {
+        N = N - Math.max(1, (N * 0.01).toInt)
+      } else {
+        // Here shaped peak is equal to measured
+        if (shape0._2 < left) {
+          D1 = D1 - Math.max(1, (D1 * 0.01).toInt)
+        }
+      }
+      //println(p0.map(_.toString).reduceLeft(_ + "\n" + _))
+    }
+  }
+
+*/
+
 
   /*
   val N = Array(1000, 10000, 100000, 1000000, 10000000, 15000000, 100000000, 1000000000)
@@ -29,10 +95,11 @@ object IntersectionTest extends App {
   //println(pd(1000, 300, 300, 10))
 
 
-  //val p1000 = p(440, 300, 300)
+  val p1000 = p(1200000, 40000, 1000)
   //println(p1000.drop(70).take(40).reduceLeft(_ + _))
-  //println(p1000.reduceLeft(_ + _) + ";\n" + p1000.map(_.toString).reduceLeft(_ + "\n" + _))
+  println(p1000.map(_.toString).reduceLeft(_ + "\n" + _))
 
+  /*
   val N = 1000
   //val nodes = new Array[Integer160](N)
   //for (i <- 0 until N) nodes(i) = Integer160.random
@@ -55,5 +122,6 @@ object IntersectionTest extends App {
     probabilities(i) = intersections.filter(_ == i).size.toDouble / Z.toDouble
   }
   println(probabilities.map(_.toString).reduceLeft(_ + "\n" + _))
+  */
 
 }

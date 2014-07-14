d89cb7cf7b413d28c81ba4e7801be467da6ac30b
Aleksey Yashchenko <tuxSlayer@gmail.com>
2014-07-14 14:25:49 +0300

    Some preparations for integration testing: useful functionality is put into a separate class, slight search plugin creation refactoring

--- test/main/scala/org/abovobo/search/SearchPluginSmokeTest2.scala
+++ test/main/scala/org/abovobo/search/SearchPluginSmokeTest2.scala
@@ -7,7 +7,7 @@ import akka.actor.Inbox
 import akka.actor.Props
 import akka.actor.ActorRef
 import org.abovobo.dht.Controller
-import org.abovobo.search.SearchPluginActor._
+import org.abovobo.search.SearchPlugin._
 import org.abovobo.integer.Integer160
 import akka.actor.ActorSystem
 import scala.concurrent.duration._
@@ -19,194 +19,82 @@ import org.abovobo.dht.DhtNode
 import scala.util.Random
 import java.io.File
 import org.abovobo.search.impl.LuceneContentIndex
+import org.abovobo.search.ContentIndex.ContentItem
+import java.nio.file.Paths
+import java.nio.file.Files
+import scala.actors.threadpool.AtomicInteger
+import org.abovobo.search.suite.SearchTestBase
 
 
 
-object SearchPluginSmokeTest2 extends App {
+object SearchPluginSmokeTest2 extends App with SearchTestBase {
+  override def debugLevel = "error"
 
-  import collection.JavaConversions._
-
-  val systemConfig = ConfigFactory.parseMap(Map(
-      "akka.log-dead-letters" -> "true", 
-      "akka.actor.debug.lifecycle" -> true,
-      "akka.loglevel" -> "error",
-      
-    "akka.actor.debug.receive" -> true,
-    "akka.actor.debug.unhandled" -> true))
-    
-  def localEndpoint(ordinal: Int) = new InetSocketAddress(InetAddress.getLocalHost, 20000 + ordinal)
-  
-  val system = ActorSystem("TestSystem", systemConfig)
-  val timeoutDuration: FiniteDuration = 7.seconds
-  implicit val timeout: Timeout = timeoutDuration
-  
-  def createNode(ordinal: Int, routers: List[InetSocketAddress] = List()): ActorRef = {
-    system.actorOf(DhtNode.props(localEndpoint(ordinal), routers), "Node-" + ordinal.toString)
-  }
-  
-  val searchIndexHome = {
-    val h = new File(System.getProperty("user.home") + "/db/search")
-    h.mkdirs
-    h
-  }
-    
-  def addSearchPlugin(node: ActorRef): ActorRef = {
-    val info = Await.result(node ? DhtNode.Describe, timeoutDuration).asInstanceOf[DhtNode.NodeInfo]
-    
-    val name = node.path.name
-    val home = new File(searchIndexHome, name).toPath()
-      
-    val searchPlugin = new SearchPlugin(
-        home, 
-        name,
-        system, 
-        info.controller, { () => info.self.id }, { () =>
-      Await.result(node ? DhtNode.Describe, 5.seconds).asInstanceOf[DhtNode.NodeInfo].nodes
-    })
-    
-    
-    searchPlugin.indexManager ! IndexManagerActor.Clear 
-    
-    searchPlugin.search 
-  }
-  
-  def printTable(node: ActorRef) {
-    val info = Await.result(node ? DhtNode.Describe, timeoutDuration).asInstanceOf[DhtNode.NodeInfo]
-    
-    println("dht table for: " + info.self.id + "@" + info.self.address)
-    println(info.nodes.size + " entries: " + info.nodes.mkString(", "))
-  }
-  
-  
-  val nodes = {
-    val ep = new InetSocketAddress(InetAddress.getLocalHost, 30000 + 1)
-    val node = DhtNode.createNode(system, ep, List(new InetSocketAddress(InetAddress.getLocalHost, 20000)))
-    val search = addSearchPlugin(node)
-    (ep, node, search) :: Nil
-  }
+  val ep = new InetSocketAddress(InetAddress.getLocalHost, 30000 + 1)
+  val node = DhtNode.createNode(system, ep, List(new InetSocketAddress(InetAddress.getLocalHost, routerPortBase)))
+  val searchPlugin = addSearchPlugin(node)
      
   Thread.sleep(1 * 1000)
   
   println("--------- find node -------")
  
-  val rnd = new Random
-  
-  rnd.shuffle(nodes).take(nodes.size) foreach { node =>
-    node._2 ! Controller.FindNode(Integer160.random)
-    Thread.sleep(1 * 1000)
-  }
+  node ! Controller.FindNode(Integer160.random)
 
-  println("--------- waiting -------")
-
-  Thread.sleep(10 * 1000)  
-
-  //for (i <- 1 to 15) 
-  {
+  Thread.sleep(1 * 1000)
     
-    println("Used Mem: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory() * 1.0 / 1024 / 1024))
-    System.gc()
-    println("Used Mem: " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory() * 1.0 / 1024 / 1024))
-    
-    println("------------------------- table")  
-    println
-    nodes.foreach { n => printTable(n._2) }
-    //Thread.sleep(60 * 1000)
-  }
+  println("--------- table -------")
+
+  printTable(node)
   
   println("--------- search test -------")
-  
-  
-  def syncask(target: ActorRef, message: Any): Any = {
-    Await.result(target ? message, 10.seconds)
-  }
 
-  val inbox = Inbox.create(system)  
-  def search(text: String)(params: SearchParams)(implicit search: ActorRef): Set[ContentIndex.ContentRef] = {
-    var result = Set.empty[ContentIndex.ContentRef]
-      
-    def receive() = {
-      try {
-        inbox.receive(timeoutDuration)
-      } catch {
-        case e: TimeoutException => e
-      }    
-    }
+  if (false) {
+    val first = ContentItem(Integer160.random.toString, "Cloud Atlas (2012)", 1025, new String(Files.readAllBytes(Paths.get("./test/test1.txt")), "UTF-8"))  
+    val second = ContentItem(Integer160.random.toString, "A Beautiful Mind (2001)", 1026, new String(Files.readAllBytes(Paths.get("./test/test2.txt")), "UTF-8"))
+    val shortOne = ContentItem(Integer160.random.toString, "short title 2", 100, "very short description")  
     
-    def recvResult(): Set[ContentIndex.ContentRef] =  { 
-      receive() match {
-        case e: TimeoutException => println("Cannot get result: " + e.getMessage); result
-        case SearchFinished(text) => result
-        case FoundItems(text, items) => 
-          result ++= items.toSet
-          recvResult()
+    searchPlugin ! SearchPlugin.Announce(first)
+    Thread.sleep(1000)
+    searchPlugin ! SearchPlugin.Announce(second)
+    Thread.sleep(1000)    
+    searchPlugin ! SearchPlugin.Announce(shortOne)
+    Thread.sleep(1 * 1000)    
+  }
+  
+  val searchers = (1 to 10).map { _ => newSearcher }
+  
+  val succeded = new AtomicInteger(0)
+  val failed = new AtomicInteger(0) 
+  val testsCount = 2000
+  val tests = (1 to testsCount).toList.toIterator
+  searchers.par.foreach { search =>
+    val tt = searchPlugin.synchronized {
+      tests.take(testsCount / searchers.size).toList      
+    }
+    tt.foreach { i =>
+      val res = search("Tom Hanks", SearchParams(3, 2), searchPlugin)
+      if (res.isEmpty) {
+        println("search: " + i + " FAILED")      
+        failed.incrementAndGet
+      } else {
+        println("search: " + i + " SUCC")            
+        succeded.incrementAndGet
       }
     }
-    
-    search.tell(Lookup(text, params), inbox.getRef)
-
-    recvResult()
-  }  
-  
-  
-//  val first = ContentItem(Integer160.random.toString, "Cloud Atlas (2012)", 1025, new String(Files.readAllBytes(Paths.get("./test/test1.txt")), "UTF-8"))  
-//  val second = ContentItem(Integer160.random.toString, "A Beautiful Mind (2001)", 1026, new String(Files.readAllBytes(Paths.get("./test/test2.txt")), "UTF-8"))
-//  val shortOne = ContentItem(Integer160.random.toString, "short title 2", 100, "very short description")  
-//  
-  val announceGroup = rnd.shuffle(nodes).take(0)
-//  
-//  announceGroup.foreach { case (ep, node, search) =>
-//    search ! SearchPlugin.Announce(first)
-//    Thread.sleep(1000)
-//    search ! SearchPlugin.Announce(second)
-//    Thread.sleep(1000)    
-//    search ! SearchPlugin.Announce(shortOne)
-//  }
-//  
-//  Thread.sleep(5 * 1000)
-//  
-//  rnd.shuffle(nodes.filterNot(announceGroup.contains(_))).take(1).foreach { case (ep, node, sp) =>
-//    def testSearch(text: String) = {
-//      println(">>>>> Starting search for: " + text)
-//      val res = search(text)(sp)
-//      println(">>>>> Search finished. Search result for " + text + ": " + res)
-//    }
-//    
-//    testSearch("Tom Hanks")
-//    testSearch("can't find anything")
-//    testSearch("\"1026 bytes\"")
-//  }  
-  
-  val sp = nodes.head._3 
-  
-  var succeded = 0
-  for (i <- 1 to 1000) {
-    val res = search("Tom Hanks")(SearchParams(3, 3))(sp)
-    if (res.isEmpty) {
-      println("search: " + i + " FAILED")      
-    } else {
-      println("search: " + i + " SUCC")            
-      succeded += 1
-    }
-    Thread.sleep(100)
   }
-  println("search test done, succ count " + succeded)
-  
-  println("------------------------- waiting")
- 
-  Thread.sleep(5 * 1000)
-
-  //for (i <- 1 to 5) 
-  while(true)
-  {
-    println("------------------------- table")  
-    nodes.foreach(n => printTable(n._2))
-    Thread.sleep(5 * 60 * 1000)
+  println("search test done, total tests: " + testsCount + ", succ: " + succeded + ", failed: " + failed + ", p = " + 1.0 * succeded.get / testsCount)
+  if (failed.get + succeded.get != testsCount) {
+    throw new IllegalStateException("Invalid tests stats: failed: " + failed.get)
   }
+  
+  Thread.sleep(1000)
+
+  println("------------------------- table")  
+  
+  printTable(node)
   
   println("------------------------- cleaning up")
-
-  Thread.sleep(1000)
-  nodes foreach { node => node._2 ! DhtNode.Stop } 
+  node ! DhtNode.Stop
   Thread.sleep(3 * 1000)
   system.shutdown()
   system.awaitTermination()
---
+++ test/main/scala/org/abovobo/search/suite/SearchTestBase.scala
@@ -0,0 +1,123 @@
+package org.abovobo.search.suite
+
+import java.io.File
+import java.net.InetAddress
+import java.net.InetSocketAddress
+
+import scala.actors.threadpool.TimeoutException
+import scala.collection.JavaConversions._
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.FiniteDuration
+import scala.util.Random
+
+import org.abovobo.dht.DhtNode
+import org.abovobo.search.ContentIndex
+import org.abovobo.search.SearchPlugin
+import org.abovobo.search.SearchPlugin.FoundItems
+import org.abovobo.search.SearchPlugin.Lookup
+import org.abovobo.search.SearchPlugin.SearchFinished
+import org.abovobo.search.SearchPlugin.SearchParams
+
+import com.typesafe.config.ConfigFactory
+
+import akka.actor.ActorRef
+import akka.actor.ActorSystem
+import akka.actor.Inbox
+import akka.pattern.ask
+import akka.util.Timeout
+import akka.util.Timeout.durationToTimeout
+
+
+trait SearchTestBase {
+  import collection.JavaConversions._
+  import scala.concurrent.duration._
+  
+  val timeoutDuration: FiniteDuration = 5.seconds
+  implicit val timeout: Timeout = timeoutDuration
+
+  def systemConfig = {
+    ConfigFactory.parseMap(Map(
+      "akka.log-dead-letters" -> "true", 
+      "akka.actor.debug.lifecycle" -> true,
+      "akka.loglevel" -> debugLevel,  
+      "akka.actor.debug.receive" -> true,
+      "akka.actor.debug.unhandled" -> true))    
+  }
+  
+  def debugLevel = "info"
+  
+  def createSystem(name: String) = ActorSystem(name, systemConfig)  
+  
+  def portBase = 20000
+  
+  def routerPortBase = 10000
+  
+  def createRouter(ordinal: Int = 0) = {
+    val routerEp = new InetSocketAddress(InetAddress.getLocalHost, routerPortBase + ordinal)
+    val router = DhtNode.createNode(system, routerEp)
+    (routerEp, router)
+  }
+  
+  def localEndpoint(ordinal: Int) = new InetSocketAddress(InetAddress.getLocalHost, portBase + ordinal)  
+  
+  def printTable(node: ActorRef) {
+    val info = Await.result(node ? DhtNode.Describe, timeoutDuration).asInstanceOf[DhtNode.NodeInfo]
+    
+    println("Dht table for: " + info.self.id + "@" + info.self.address)
+    println(info.nodes.size + " entries: " + info.nodes.mkString(", "))
+  }  
+  
+  def searchIndexHome = new File(System.getProperty("user.home") + "/db/search")
+
+  def addSearchPlugin(node: ActorRef): ActorRef = {
+    val info = Await.result(node ? DhtNode.Describe, timeoutDuration).asInstanceOf[DhtNode.NodeInfo]
+    
+    val name = node.path.name
+    val home = new File(searchIndexHome, name).toPath()
+      
+    val search = SearchPlugin(
+        home, 
+        name,
+        system, 
+        info.controller, { () => info.self.id }, { () =>
+      Await.result(node ? DhtNode.Describe, timeoutDuration).asInstanceOf[DhtNode.NodeInfo].nodes
+    })
+
+    search
+  }
+    
+  def newSearcher = {
+    val inbox = Inbox.create(system)  
+    def search(text: String, params: SearchParams, searchPlugin: ActorRef): Set[ContentIndex.ContentRef] = {
+      var result = Set.empty[ContentIndex.ContentRef]
+        
+      def receive() = {
+        try {
+          inbox.receive(timeoutDuration)
+        } catch {
+          case e: TimeoutException => e
+        }    
+      }
+      
+      def recvResult(): Set[ContentIndex.ContentRef] =  { 
+        receive() match {
+          case e: TimeoutException => println("Cannot get result: " + e.getMessage); result
+          case SearchFinished(text) => result
+          case FoundItems(text, items) => 
+            result ++= items.toSet
+            recvResult()
+        }
+      }
+      
+      searchPlugin.tell(Lookup(text, params), inbox.getRef)
+  
+      recvResult()
+    }
+    search _    
+  }
+
+  
+  lazy val system = createSystem("TestSystem")
+  val rnd = new Random
+}
